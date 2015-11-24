43b7bea75a812b4d53a8bf113e552122a7ab6967
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2015-11-24 15:05:33 +0100

    New methods and code refactoring
    Implementation of methods getAll and addProcess
    methods.

--- process/src/main/java/de/unimannheim/spa/process/persistence/ProjectRepository.java
+++ process/src/main/java/de/unimannheim/spa/process/persistence/ProjectRepository.java
@@ -1,7 +1,8 @@
 package de.unimannheim.spa.process.persistence;
 
 import de.unimannheim.spa.process.domain.Project;
+import de.unimannheim.spa.process.domain.Process;
 
 public interface ProjectRepository extends Repository<String, Project> {
-
+  public boolean addProcess(String projectID, Process process);
 }
--- process/src/main/java/de/unimannheim/spa/process/persistence/inmemory/InMemoryProjectRepository.java
+++ process/src/main/java/de/unimannheim/spa/process/persistence/inmemory/InMemoryProjectRepository.java
@@ -1,6 +1,7 @@
 package de.unimannheim.spa.process.persistence.inmemory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -8,30 +9,48 @@ import org.springframework.stereotype.Component;
 import com.google.common.collect.Lists;
 
 import de.unimannheim.spa.process.domain.Project;
+import de.unimannheim.spa.process.domain.Process;
 import de.unimannheim.spa.process.persistence.ProjectRepository;
 
 @Component
 @Profile("development")
 public class InMemoryProjectRepository implements ProjectRepository {
 
-	private final List<Project> memory;
+	private List<Project> memory;
 
 	public InMemoryProjectRepository() {
-		this(Lists.newArrayList());
+		this(InMemoryOps.create5ProjectsWith5ProcessesEach());
 	}
 
 	public InMemoryProjectRepository(List<Project> init) {
-		this.memory = init;
+	    this.memory = Lists.newArrayList();
+	    init.stream().forEach(project -> memory.add(project));
 	}
 
 	@Override
 	public boolean deleteById(String id) {
-
+	  return (memory.removeIf(project -> project.getId().equalsIgnoreCase(id)));
 	}
 
 	@Override
 	public boolean save(Project entity) {
-		return false;
+		return (memory.stream().noneMatch(project -> project.getId().equalsIgnoreCase(entity.getId()))) ? memory.add(entity) : false;
 	}
 
+    @Override
+    public List<Project> getAll() {
+        return memory;
+    }
+
+    @Override
+    public boolean addProcess(String projectID, Process process) {
+      if(memory.stream().anyMatch(project -> project.getId().equals(projectID))){
+        memory.stream().filter(project -> project.getId().equals(projectID)).map(project -> {
+          return project.getProcesses().add(process);
+        }).collect(Collectors.toList());
+        return true;
+      }
+      return false;
+    }
+    
 }
