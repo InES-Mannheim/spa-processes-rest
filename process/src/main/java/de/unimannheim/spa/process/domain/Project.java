bffc79717c75ee426e1a3952f54424eea1165568
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2015-11-24 14:52:49 +0100

    ProjectService implementation
    ProjectService code was refactored removing the local
    Map where the projects were stored. Now the service is 
    using the ProjectRepository instead of local Map.

--- process/src/main/java/de/unimannheim/spa/process/domain/Project.java
+++ process/src/main/java/de/unimannheim/spa/process/domain/Project.java
@@ -2,6 +2,8 @@ package de.unimannheim.spa.process.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.Lists;
 
 public class Project implements Entity {
@@ -12,9 +14,29 @@ public class Project implements Entity {
 	
 	private List<Process> processes;
 	
+	@SuppressWarnings("unused")
+    private int processNumber;
+	
 	public Project(String id) {
 		this.id = id;
+		this.processes = Lists.newArrayList();
+		this.label = String.join("-", id, "label");
 	}
+	
+	public Project(String id, List<Process> processes){
+	    this(id);
+	    if(processes != null){
+	      this.processes.addAll(processes); 
+	    } else {
+	      this.processes = Lists.newArrayList();
+	      this.processes.addAll(processes);
+	    }
+	}
+	
+	public Project(String id, String label, List<Process> processes){
+	    this(id, processes);
+	    this.label = label;
+    }
 
 	@Override
 	public String getId() {
@@ -29,10 +51,12 @@ public class Project implements Entity {
 		this.label = label;
 	}
 
+	@JsonIgnore
 	public List<Process> getProcesses() {
 		return processes;
 	}
 
+	@JsonProperty
 	public void setProcesses(List<Process> processes) {
 		this.processes = processes;
 	}
@@ -41,5 +65,9 @@ public class Project implements Entity {
 		if(process == null) processes = Lists.newArrayList();
 		return processes.add(process);
 	}
+
+    public int getProcessNumber() {
+      return processes.size();
+    }
 
 }
--- process/src/main/java/de/unimannheim/spa/process/service/ProjectService.java
+++ process/src/main/java/de/unimannheim/spa/process/service/ProjectService.java
@@ -1,8 +1,6 @@
 package de.unimannheim.spa.process.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,15 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
 
-import de.unimannheim.spa.process.domain.Source;
+import de.unimannheim.spa.process.domain.Project;
+import de.unimannheim.spa.process.domain.Process;
 import de.unimannheim.spa.process.persistence.ProjectRepository;
 
 @Service
 public class ProjectService {
 
-	private Map<String, Map<String, Source>> projects;
 	private final ProjectRepository repo;
 
 	@Autowired
@@ -29,27 +26,17 @@ public class ProjectService {
 	public boolean deleteById(String processId) {
 		Preconditions.checkNotNull(processId, "Process Id must not be null.");
 		Preconditions.checkArgument(!processId.isEmpty(), "Process Id must not be empty.");
-		
 		return repo.deleteById(processId);
-//		for (Entry<String, Map<String, Source>> entryProject : projects.entrySet()) {
-//			if (entryProject.getValue().containsKey(processId)) {
-//				projects.remove(entryProject.getKey());
-//				return true;
-//			}
-//		}
-//		return false;
 	}
 
-	public List<String> listAll() {
-		List<String> dummyProjectIDList = new ArrayList<String>();
-		dummyProjectIDList.addAll(projects.keySet());
-		return dummyProjectIDList;
+	public List<Project> listAll() {
+		return repo.getAll();
 	}
 
 	public String create(ProjectType type) {
 		Preconditions.checkNotNull(type, "Type must not be null.");
 		final String generatedProjectID = this.generateProjectId();
-		projects.put(generatedProjectID, Maps.newHashMap());
+		repo.save(new Project(generatedProjectID));
 		return generatedProjectID;
 	}
 
@@ -57,12 +44,11 @@ public class ProjectService {
 		return "Project-" + UUID.randomUUID();
 	}
 
-	public boolean add(String projectId, ProjectType type) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean addProcess(String projectId, Process process, ProjectType type) {
+		return repo.addProcess(projectId, process);
 	}
 
-	public Optional<String> findById(String projectId) {
-		return listAll().stream().filter(projectId::equals).findFirst();
+	public Optional<Project> findById(String projectId) {
+		return listAll().stream().filter(project -> project.getId().equals(projectId)).findFirst();
 	}
 }
