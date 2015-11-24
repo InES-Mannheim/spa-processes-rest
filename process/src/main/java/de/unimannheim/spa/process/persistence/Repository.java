dd71bc21e79ca3f93be8f2a435700a692518ea65
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2015-11-24 15:01:20 +0100

    Add method getAll

--- process/src/main/java/de/unimannheim/spa/process/persistence/Repository.java
+++ process/src/main/java/de/unimannheim/spa/process/persistence/Repository.java
@@ -1,6 +1,9 @@
 package de.unimannheim.spa.process.persistence;
 
+import java.util.List;
+
 public interface Repository<I,T> {
 	public boolean deleteById(I id);
 	public boolean save(T entity);
+	public List<T> getAll();
 }
