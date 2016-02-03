d6072a87fbac88e361b7800e6f48127c777bfb85
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2016-02-03 23:11:40 +0100

    Add Apache licence to all files

--- process/src/test/java/de/unimannheim/spa/process/rest/ProjectRestControllerTest.java
+++ process/src/test/java/de/unimannheim/spa/process/rest/ProjectRestControllerTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2016 University of Mannheim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package de.unimannheim.spa.process.rest;
 
 import static org.hamcrest.core.Is.isA;
@@ -7,18 +22,14 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.junit.Assert.assertThat;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
